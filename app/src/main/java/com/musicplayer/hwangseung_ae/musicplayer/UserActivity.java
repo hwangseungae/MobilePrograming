03dc62b855432c02878f9d691f76eb1ce03989ba
SeungAe <hwangseung-ae@hwangseung-aeui-MacBook-Pro.local>
2018-12-14 12:12:08 +0900

    Implement Plus Meno Activity

--- app/src/main/java/com/musicplayer/hwangseung_ae/musicplayer/UserActivity.java
+++ app/src/main/java/com/musicplayer/hwangseung_ae/musicplayer/UserActivity.java
@@ -1,7 +1,11 @@
 package com.musicplayer.hwangseung_ae.musicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        final ImageButton musicButton = (ImageButton) findViewById(R.id.plus);
+
+        musicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                newsong.setVisibility(View.GONE);
+//                musicButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+//                videoButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+//                myButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                Intent intent = new Intent(getApplicationContext(), MemoActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
