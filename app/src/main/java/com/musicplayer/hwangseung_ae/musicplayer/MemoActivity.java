d1eb30973ce8599b62c4b6e0c86d80b1368dec0f
SeungAe <hwangseung-ae@hwangseung-aeui-MacBook-Pro.local>
2018-12-14 17:32:17 +0900

    Implement Memo Write&Save&Delete

--- app/src/main/java/com/musicplayer/hwangseung_ae/musicplayer/MemoActivity.java
+++ app/src/main/java/com/musicplayer/hwangseung_ae/musicplayer/MemoActivity.java
@@ -2,12 +2,48 @@ package com.musicplayer.hwangseung_ae.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MemoActivity extends AppCompatActivity {
+
+    EditText mMenoEdit = null;
+    TextFileManager mTextFileManager = new TextFileManager(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo);
+
+        mMenoEdit = (EditText)findViewById(R.id.memo_edit);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.load_btn: {
+                String memoData = mTextFileManager.load();
+                mMenoEdit.setText(memoData);
+
+                Toast.makeText(this, "불러오기 완료", Toast.LENGTH_LONG).show();
+                break;
+            }
+
+            case R.id.save_btn: {
+                String memoData = mMenoEdit.getText().toString();
+                mTextFileManager.save(memoData);
+                mMenoEdit.setText("");
+
+                Toast.makeText(this, "저장 완료", Toast.LENGTH_LONG).show();
+                break;
+            }
+
+            case R.id.delete_btn: {
+                mTextFileManager.delete();
+                mMenoEdit.setText("");
+                Toast.makeText(this, "삭제 완료", Toast.LENGTH_LONG).show();
+
+            }
+        }
     }
 }
