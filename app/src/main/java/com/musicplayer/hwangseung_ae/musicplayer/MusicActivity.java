b182ee575db9e19f2e9f42158b8d3990f2dd66d1
SeungAe <hwangseung-ae@hwangseung-aeui-MacBook-Pro.local>
2018-12-16 23:20:16 +0900

    Add MusicActivity UI

--- app/src/main/java/com/musicplayer/hwangseung_ae/musicplayer/MusicActivity.java
+++ app/src/main/java/com/musicplayer/hwangseung_ae/musicplayer/MusicActivity.java
@@ -1,163 +1,177 @@
 package com.musicplayer.hwangseung_ae.musicplayer;
 
-//import android.Manifest;
-//import android.content.pm.PackageManager;
+
+//import android.content.Intent;
 //import android.database.Cursor;
-//import android.net.Uri;
-//import android.nfc.Tag;
-//import android.os.Build;
-//import android.provider.MediaStore;
-//import android.support.annotation.NonNull;
-//import android.support.v4.app.ActivityCompat;
-//import android.support.v4.app.FragmentActivity;
-//import android.support.v4.app.LoaderManager;
-//import android.support.v4.content.CursorLoader;
-//import android.support.v4.content.Loader;
-//import android.support.v7.app.AppCompatActivity;
 //import android.os.Bundle;
-//import android.support.v7.widget.LinearLayoutManager;
-//import android.support.v7.widget.RecyclerView;
-//import android.util.Log;
+//import android.provider.MediaStore;
+//import android.support.v7.app.AppCompatActivity;
+//import android.view.View;
+//import android.widget.AdapterView;
 //import android.widget.ListView;
 //
-//public class MusicActivity extends AppCompatActivity {
-//    private final static int LOADER_ID = 0x001;
+//import java.util.ArrayList;
 //
-//    private RecyclerView mRecyclerView;
-//    private AudioAdapter mAdapter;
+//public class MusicActivity extends AppCompatActivity {
+//    private ListView listView;
+//    public static ArrayList<MusicDto> list;
+//
 //
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_music);
+//        getMusicList(); // 디바이스 안에 있는 mp3 파일 리스트를 조회하여 LIst를 만듭니다.
+//        listView = (ListView)findViewById(R.id.listview);
+//        MyAdapter adapter = new MyAdapter(this,list);
+//        listView.setAdapter(adapter);
 //
-//        // OS가 Marshmallow 이상일 경우 권한체크를 해야 합니다.
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
-//            } else {
-//                // READ_EXTERNAL_STORAGE 에 대한 권한이 있음.
-//                getAudioListFromMediaDatabase();
-//            }
-//        }
-//        // OS가 Marshmallow 이전일 경우 권한체크를 하지 않는다.
-//        else {
-//            getAudioListFromMediaDatabase();
-//        }
 //
-//        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-//        mAdapter = new AudioAdapter(this, null);
-//        mRecyclerView.setAdapter(mAdapter);
-//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-//        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-//        mRecyclerView.setLayoutManager(layoutManager);
-//    }
-//
-//    private void getAudioListFromMediaDatabase() {
-//        getSupportLoaderManager().initLoader(LOADER_ID, null, new LoaderManager.LoaderCallbacks<Cursor>() {
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            @Override
-//            public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-//                Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-//                String[] projection = new String[]{
-//                        MediaStore.Audio.Media._ID,
-//                        MediaStore.Audio.Media.TITLE,
-//                        MediaStore.Audio.Media.ARTIST,
-//                        MediaStore.Audio.Media.ALBUM,
-//                        MediaStore.Audio.Media.ALBUM_ID,
-//                        MediaStore.Audio.Media.DURATION,
-//                        MediaStore.Audio.Media.DATA
-//                };
-//                String selection = MediaStore.Audio.Media.IS_MUSIC + " = 1";
-//                String sortOrder = MediaStore.Audio.Media.TITLE + " COLLATE LOCALIZED ASC";
-//                return new CursorLoader(getApplicationContext(), uri, projection, selection, null, sortOrder);
-//            }
-//
-//            @Override
-//            public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-//                mAdapter.swapCursor(data);
-//            }
-//
-//            @Override
-//            public void onLoaderReset(Loader<Cursor> loader) {
-//                mAdapter.swapCursor(null);
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Intent intent = new Intent(MusicActivity.this,PlayingActivity.class);
+//                intent.putExtra("position",position);
+//                intent.putExtra("playlist",list);
+//                startActivity(intent);
 //            }
 //        });
 //    }
 //
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//            // READ_EXTERNAL_STORAGE 에 대한 권한 획득.
-//            getAudioListFromMediaDatabase();
+//
+//    public  void getMusicList(){
+//        list = new ArrayList<>();
+//        //가져오고 싶은 컬럼 명을 나열합니다. 음악의 아이디, 앰블럼 아이디, 제목, 아스티스트 정보를 가져옵니다.
+//        String[] projection = {MediaStore.Audio.Media._ID,
+//                MediaStore.Audio.Media._ID,
+//                MediaStore.Audio.Media.ALBUM_ID,
+//                MediaStore.Audio.Media.TITLE,
+//                MediaStore.Audio.Media.ALBUM,
+//                MediaStore.Audio.Media.ARTIST,
+//                MediaStore.Audio.Media.DATA
+//        };
+//
+//        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+//                projection, null, null, null);
+//
+//        while(cursor.moveToNext()){
+//            MusicDto musicDto = new MusicDto();
+//            musicDto.setId(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA)));
+//            musicDto.setId(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID)));
+//            musicDto.setAlbumId(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM)));
+//            musicDto.setAlbumId(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)));
+//            musicDto.setTitle(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE)));
+//            musicDto.setArtist(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST)));
+//            list.add(musicDto);
 //        }
+//        cursor.close();
 //    }
 //}
 
 
-import android.content.Intent;
-import android.database.Cursor;
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.Button;
 
-import java.util.ArrayList;
+public class MusicActivity extends Activity {
+    MediaPlayer mp;
 
-public class MusicActivity extends AppCompatActivity {
-    private ListView listView;
-    public static ArrayList<MusicDto> list;
+    // 시작버튼
+    Button startButton;
+    //종료버튼
+    Button stopButton;
+
+    int playstopBtns[] = {R.id.startme, R.id.stopme, R.id.startbackwalk, R.id.stopbackwalk, R.id.startcry, R.id.stopcry,
+            R.id.startcure, R.id.stopcure, R.id.startluv, R.id.stopluv, R.id.startrelove, R.id.stoprelove};
+
+    int resMp3[] = {R.raw.mino, R.raw.twice, R.raw.ben, R.raw.shaun, R.raw.heize, R.raw.jennie};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
-        getMusicList(); // 디바이스 안에 있는 mp3 파일 리스트를 조회하여 LIst를 만듭니다.
-        listView = (ListView)findViewById(R.id.listview);
-        MyAdapter adapter = new MyAdapter(this,list);
-        listView.setAdapter(adapter);
 
+//        for (int btns : playstopBtns) {
+//            Button btn = (Button) findViewById(btns);
+//            btn.setOnClickListener(startButton);
+//        }
+        startButton = findViewById(R.id.startme);
+        stopButton = findViewById(R.id.stopme);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MusicActivity.this,PlayingActivity.class);
-                intent.putExtra("position",position);
-                intent.putExtra("playlist",list);
-                startActivity(intent);
+            public void onClick(View view) {
+                // MediaPlayer 객체 할당
+                mp = MediaPlayer.create(MusicActivity.this, R.raw.mino);
+                mp.start();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 정지버튼
+                mp.stop();
+                // 초기화
+                mp.reset();
             }
         });
     }
 
 
-    public  void getMusicList(){
-        list = new ArrayList<>();
-        //가져오고 싶은 컬럼 명을 나열합니다. 음악의 아이디, 앰블럼 아이디, 제목, 아스티스트 정보를 가져옵니다.
-        String[] projection = {MediaStore.Audio.Media._ID,
-                MediaStore.Audio.Media._ID,
-                MediaStore.Audio.Media.ALBUM_ID,
-                MediaStore.Audio.Media.TITLE,
-                MediaStore.Audio.Media.ALBUM,
-                MediaStore.Audio.Media.ARTIST,
-                MediaStore.Audio.Media.DATA
-        };
-
-        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                projection, null, null, null);
-
-        while(cursor.moveToNext()){
-            MusicDto musicDto = new MusicDto();
-            musicDto.setId(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA)));
-            musicDto.setId(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID)));
-            musicDto.setAlbumId(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM)));
-            musicDto.setAlbumId(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)));
-            musicDto.setTitle(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE)));
-            musicDto.setArtist(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST)));
-            list.add(musicDto);
-        }
-        cursor.close();
-    }
 }
+
+//    View.OnClickListener clickListener = new View.OnClickListener() {
+//
+//        public void onClick(View v) {
+//
+//            switch (v.getId()) {
+//
+//                case R.id.startme:
+//                    Play(0); break;
+//                case R.id.startbackwalk:
+//                    Play(1); break;
+//                case R.id.startcry:
+//                    Play(2); break;
+//                case R.id.startcure:
+//                    Play(3); break;
+//                case R.id.startluv:
+//                    Play(4); break;
+//                case R.id.startrelove:
+//                    Play(5); break;
+//                default :
+//                    Stop();
+//            }
+//
+//        }
+//
+//    };
+//
+//
+//    private void Play(int selNo){
+//
+//        Stop();
+//
+//        mp = MediaPlayer.create(MusicActivity.this, resMp3[selNo]);
+//        mp.start();
+//    }
+//
+//    private void Stop(){
+//
+//        if(mp!=null){
+//            mp.stop();
+//            mp = null;
+//        }
+//
+//    }
+//
+//    @Override
+//    public void onDestroy() {
+//        super.onDestroy();
+//        Stop();
+//    }
+//
+//}
