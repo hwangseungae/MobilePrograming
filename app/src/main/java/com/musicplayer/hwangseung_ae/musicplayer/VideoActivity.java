2c832bade1342d6b1164351dc79041890deda725
SeungAe <hwangseung-ae@hwangseung-aeui-MacBook-Pro.local>
2018-12-16 09:52:49 +0900

    Implement ActionButton

--- app/src/main/java/com/musicplayer/hwangseung_ae/musicplayer/VideoActivity.java
+++ app/src/main/java/com/musicplayer/hwangseung_ae/musicplayer/VideoActivity.java
@@ -1,13 +1,68 @@
 package com.musicplayer.hwangseung_ae.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class VideoActivity extends AppCompatActivity {
+
+    ImageButton but1;
+    ImageButton but2;
+    ImageButton but3;
+    ImageButton but4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        but1 = (ImageButton) findViewById(R.id.naver);
+        but2 = (ImageButton) findViewById(R.id.daum);
+        but3 = (ImageButton) findViewById(R.id.youtube);
+        but4 = (ImageButton) findViewById(R.id.sprots);
+
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.naver.com"));
+                startActivity(intent);
+            }
+        });
+
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.daum.net"));
+                startActivity(intent);
+            }
+        });
+
+        but3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.youtube.com"));
+                startActivity(intent);
+            }
+        });
+
+        but4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.naversport.com"));
+                startActivity(intent);
+            }
+        });
     }
 }
