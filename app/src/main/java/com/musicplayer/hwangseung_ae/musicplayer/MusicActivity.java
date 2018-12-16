8ef54676af9cce42b112f642f89591abfa579aeb
SeungAe <hwangseung-ae@hwangseung-aeui-MacBook-Pro.local>
2018-12-17 03:30:58 +0900

    Modified MusicPlayr UI & MediaPlayer

--- app/src/main/java/com/musicplayer/hwangseung_ae/musicplayer/MusicActivity.java
+++ app/src/main/java/com/musicplayer/hwangseung_ae/musicplayer/MusicActivity.java
@@ -77,7 +77,7 @@ import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -87,7 +87,7 @@ import java.io.IOException;
 
 public class MusicActivity extends Activity {
 
-    Button btn1, btn2, btn3;
+    ImageButton btn1, btn2, btn3;
     MediaPlayer mp;
     SeekBar seekBar;
     TextView text;
@@ -98,14 +98,14 @@ public class MusicActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
 
-        btn1 = (Button)findViewById(R.id.button1);
-        btn2 = (Button)findViewById(R.id.button2);
-        btn3 = (Button)findViewById(R.id.button3);
+        btn1 = (ImageButton)findViewById(R.id.button1);
+        btn2 = (ImageButton)findViewById(R.id.button2);
+        btn3 = (ImageButton)findViewById(R.id.button3);
 
         text = (TextView)findViewById(R.id.text1);
 
 
-        mp = MediaPlayer.create(MusicActivity.this, R.raw.test);
+        mp = MediaPlayer.create(MusicActivity.this, R.raw.ben);
 
         seekBar = (SeekBar)findViewById(R.id.playbar);
         seekBar.setVisibility(ProgressBar.VISIBLE);
