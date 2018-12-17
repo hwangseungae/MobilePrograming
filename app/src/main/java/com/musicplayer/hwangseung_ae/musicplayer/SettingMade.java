3f12587f973505fa6fa92ab1dc083af7bb5e2a97
SeungAe <hwangseung-ae@hwangseung-aeui-MacBook-Pro.local>
2018-12-18 04:08:50 +0900

    Implement SettingActivity

---
+++ app/src/main/java/com/musicplayer/hwangseung_ae/musicplayer/SettingMade.java
@@ -0,0 +1,14 @@
+package com.musicplayer.hwangseung_ae.musicplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingMade extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_made);
+    }
+
+}
